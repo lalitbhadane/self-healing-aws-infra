54665258a4fb4095cfdce8bb81a368e0797c342f
Lalit Bhadane <lalitbhadane7020@gmail.com>
2026-09-09 18:27:52 +0000

    CI: Configure terraform remote state and locking

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+    bucket	 = "self-healing-aws-infra-terraform-state-65b98806"
+    key          = "self-healing-aws-infra/terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
